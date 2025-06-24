d8f510eda5896d486d93a37c4c37786f22d82ff5
Thiago <thiagopaulista50@gmail.com>
2025-06-23 22:18:56 -0300

    feat: adding repository

--- app/src/main/java/com/orama/app/model/Transaction.java
+++ app/src/main/java/com/orama/app/model/Transaction.java
@@ -1,8 +1,17 @@
 package com.orama.app.model;
 
-import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "transaction")
@@ -26,8 +35,8 @@ public class Transaction {
     private Category category;
 
     @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
 
     private LocalDate createdAt = LocalDate.now();
 
@@ -46,8 +55,8 @@ public class Transaction {
     public Category getCategory() { return category; }
     public void setCategory(Category category) { this.category = category; }
 
-    public User getUser() { return user; }
-    public void setUser(User user) { this.user = user; }
+    public Client getClient() { return client; }
+    public void setClient(Client client) { this.client = client; }
 
     public LocalDate getCreatedAt() { return createdAt; }
     public void setCreatedAt(LocalDate createdAt) { this.createdAt = createdAt; }
---
+++ app/src/main/java/com/orama/app/repository/ClientRepository.java
@@ -0,0 +1,8 @@
+package com.orama.app.repository;
+
+import com.orama.app.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long>{
+
+}
