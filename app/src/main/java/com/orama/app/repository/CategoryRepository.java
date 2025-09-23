121bb147b86817d0b2bd54a1f8e09e38ab5f1720
Thiago <thiagopaulista50@gmail.com>
2025-09-23 20:45:56 -0300

    feat(core): implement business logic services and repositories

--- app/src/main/java/com/orama/app/repository/CategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.orama.app.repository;
-
-import com.orama.app.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {}
